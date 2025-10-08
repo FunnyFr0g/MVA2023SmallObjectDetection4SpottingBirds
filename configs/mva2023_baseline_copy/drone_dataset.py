678463530ec0b025c342b5d48ec892cb9f23eba6
ilya_s <i.n.lagutin@yandex.ru>
2025-10-08 18:14:47 +0000

    workers

--- configs/mva2023_baseline_copy/drone_dataset.py
+++ configs/mva2023_baseline_copy/drone_dataset.py
@@ -61,7 +61,7 @@ test_pipeline = [
 
 data = dict(
     samples_per_gpu=16,
-    workers_per_gpu=4,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file= '/clearml_agent_cache/storage_manager/datasets/ds_ae8c12c33b324947af9ae6379d920eb8/train_annotations.json',
--- my_setup.sh
+++ my_setup.sh
@@ -1,3 +1,6 @@
+apt-get update
+apt-get install -y cuda-toolkit-11-8
+
 pip install -U openmim
 mim install mmcv-full==1.6.0
 pip install matplotlib
@@ -8,5 +11,8 @@ pip install -v -e .
 apt update
 apt install -y libgl1-mesa-glx libglib2.0-0
 
-mkdir -p work_dirs/centernet_resnet18_140e_coco1
-touch work_dirs/centernet_resnet18_140e_coco1/latest.pth
+mkdir -p work_dirs/centernet_resnet18_140e_coco
+touch work_dirs/centernet_resnet18_140e_coco/latest.pth
+
+pip install yapf==0.30.0
+
