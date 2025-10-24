aae52c82661600ce669a21f31654b94696f6f6aa
ILYA-S <i.n.lagutin@yandex.ru>
2025-10-24 09:04:35 +0000

    fix: upload_artifact

--- upload_artifact.py
+++ upload_artifact.py
@@ -2,8 +2,12 @@ from clearml import Task
 
 task = Task.get_task(task_id='defc556e31e8404184ecdfa0ed4dd9cb')
 
+task.started()
+
 task.upload_artifact(
     name='finetuning work dir',
-    artifact_object='/root/mva23/MVA2023SmallObjectDetection4SpottingBirds/work_dirs'
+    artifact_object='/root/mva23/MVA2023SmallObjectDetection4SpottingBirds/work_dirs/centernet_resnet18_140e_coco_finetune'
 )
 print('Артефакт загружен')
+
+task.mark_completed()
